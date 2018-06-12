0694bfd6ffb74b8a11bde12dd254640a8a3579a4
Lazy-Y <zhenyanz@usc.edu>
2018-06-12 10:59:31 -0700

    auth request

--- main.go
+++ main.go
@@ -8,43 +8,6 @@ import (
 	"strings"
 )
 
-// func exempt_route(urls map[string]string) {
-// 	for suburl, _ := range urls {
-// 		http.Handle(suburl+"exempt/", &httputil.ReverseProxy{
-// 			Director: func(r *http.Request) {
-// 				fmt.Println("exempt", r.URL.Path, suburl)
-// 				r.URL.Scheme = "http"
-// 				host := urls[suburl]
-// 				r.URL.Host = host
-// 				r.URL.Path = strings.Replace(r.URL.Path, suburl, "/", 1)
-// 				fmt.Println("after", r.URL.Path, host)
-// 			}})
-// 	}
-// }
-
-// func auth_route(urls map[string]string) {
-// 	for suburl, host := range urls {
-// 		fmt.Println("URL", suburl, host)
-// 		http.Handle(suburl, &httputil.ReverseProxy{
-// 			Director: func(r *http.Request) {
-// 				fmt.Println("auth", r.URL.Path, suburl)
-// 				r.URL.Scheme = "http"
-// 				host := urls[suburl]
-// 				r.URL.Host = host
-// 				r.URL.Path = strings.Replace(r.URL.Path, suburl, "/", 1)
-// 				fmt.Println("after", r.URL.Path, host)
-// 			}})
-// 	}
-// }
-
-// func default_handler(rw http.ResponseWriter, req *http.Request) {
-// 	fmt.Fprintf(rw, req.URL.Path)
-// }
-
-// func handler(rw http.ResponseWriter, req *http.Request) {
-// 	fmt.Fprintf(rw, req.URL.Path)
-// }
-
 func main() {
 	urls := make(map[string]string)
 	for _, env := range os.Environ() {
@@ -56,9 +19,6 @@ func main() {
 			urls[suburl] = host
 		}
 	}
-	fmt.Println(urls)
-	// exempt_route(urls)
-	// auth_route(urls)
 	http.Handle("/", &httputil.ReverseProxy{
 		Director: func(r *http.Request) {
 			fmt.Println("base url", r.URL.Path)
@@ -68,11 +28,16 @@ func main() {
 					fmt.Println("auth", r.URL.Path, suburl)
 					r.URL.Host = host
 					r.URL.Path = strings.Replace(r.URL.Path, suburl, "/", 1)
+					if strings.HasPrefix(r.URL.Path, "/exempt/"){
+						fmt.Println("Exempting request")
+					}
+					else{
+						fmt.Println("Auth request")
+					}
 					fmt.Println("after", r.URL.Path, host)
 					break
 				}
 			}
 		}})
-	// http.HandleFunc("/", default_handler)
 	http.ListenAndServe(":8000", nil)
 }
